dacf14dda12f3cbfdda6cd3a3ffd41046d433169
MohamedAkram244 <124067586+MohamedAkram244@users.noreply.github.com>
2023-01-31 17:43:55 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int N;
+    printf(" Enter the Number : ");
+    scanf("%d",&N);
+    if (N % 2 ==0)
+    {
+        printf(" the number is even ");
+    }
+    else
+    {
+        printf(" the number is odd ");
+    }
+    return 0;
+}
